7508d0c0de6c0cd08b7f06886c315ab1667c0309
Mirko Tritella <mirkotritella1999@gmail.com>
2021-12-22 20:40:36 +0100

    Started scraping, first 1200 anime scraped

--- Web Scaping Code/webScraping.py
+++ Web Scaping Code/webScraping.py
@@ -13,22 +13,17 @@ from bs4 import BeautifulSoup
 def getPage(malId):
     URL = "https://myanimelist.net/anime/" + str(malId)
     page = requests.get(URL)
-    return page.status_code, page.url, page.content
+    return page.status_code, page.url, BeautifulSoup(page.content, "html.parser")
+
 
 # Get the list of MAL ID of the anime from csv file
 def getMalIds():
     ids = []
-    with open("../data/api/anime_informations.csv", "r") as file:
+    with open("../data/api/animeid.csv", "r") as file:
         for line in file:
             animeTitle = line.strip().split(",")[0]
             ids.append(animeTitle)
     return ids
-
-
-# Mantain only the even indices in the list
-def even(list):
-    return [list[i] for i in range(0, len(list), 2)]
-
 
 # Create a csv file with the informations of the anime
 def informationsCsv(id, animeStudios, producer):
@@ -38,20 +33,27 @@ def informationsCsv(id, animeStudios, producer):
 
 # Scrape the informations about animeStudios and Producer of the content of response
 def getStudios(response):
-    soup = BeautifulSoup(response, "html.parser")
-    studio = soup.find_all("span", class_="dark_text")
-    for i in studio:
-        if i.text == "Studios:":
-            animeStudios = i.next_sibling.next_sibling.text
-        elif i.text == "Source:":
-            source = i.next_sibling.text
+    try:
+        studio = response.find_all("span", class_="dark_text")
+        for i in studio:
+            if i.text == "Studios:":
+                animeStudios = i.next_sibling.next_sibling.text.replace("\n", "").replace(",", " ")
+        
+        if animeStudios == "add some":
+            animeStudios = ""
+    except:
+        animeStudios = ""
+        print("Error in studios for id: " + str(id))
     return animeStudios
 
 # Scrape the first element in the list of staff in content of response
 def getProducer(response):
-    soup = BeautifulSoup(response, "html.parser")
-    staffDiv = soup.find_all("div", class_="detail-characters-list clearfix")
-    producer = staffDiv[1].find_all("img")[0].get("alt")
+    try:
+        staffDiv = response.find_all("div", class_="detail-characters-list clearfix")
+        producer = staffDiv[1].find_all("img")[0].get("alt")
+    except:
+        producer = ""
+        print("Error in producer for id: " + str(id))
     
     return producer.replace(",", "")
 
@@ -65,19 +67,50 @@ def checkErrors(statusCode, url, id):
     else:
         return True
 
-# Get reviews from the page of the anime
-#def getReviews(response)
-
 
 # Get recommendations from the page of the anime broken
-def getRecommendations(response):
-    soup = BeautifulSoup(response, "html.parser")
-    recommendations = soup.find_all("div", class_="recommendations-container")
-    recommendations = recommendations[0].find_all("a")
-    recommendations = [recommendation.get("href") for recommendation in recommendations]
-    recommendations = [recommendation.split("/")[2] for recommendation in recommendations]
-    recommendations = even(recommendations)
-    print(recommendations)
+def getRecommendations(id, response):
+    try:
+        # Get the list of div with class btn-anime
+        recommendationList = response.find_all("li", class_="btn-anime")
+        # Get the alt of every url in recommendations img
+        recommendationsURLs = [recommendation.find("a").get("href") for recommendation in recommendationList]
+
+        # Get the ids of the raccomendated animes
+        recommendationsIDs = []
+        for url in recommendationsURLs:
+            if '/recommendations/' in url:
+                urls = url.split('/')[5].split('-')
+                urls.remove(id)
+                recommendationsIDs.append(urls[0])
+            else:
+                recommendationsIDs.append(url.split('/')[4])
+    except:
+        recommendationsIDs = []
+        print("Error in recommendations for id: " + str(id))
+    return recommendationsIDs
+
+# Get the reviews and votes of the anime
+def getRatingAndReviews(response):
+    try:
+        # Get the votes
+        rating = response.find_all("div", class_="mb8")
+        votes = []
+        for vote in rating:
+            votes.append(vote.text.split(":")[1].replace("\n", "").replace(" ", ""))
+
+        # Get the reviews text
+        reviews = response.find_all("div", class_="textReadability")
+        reviewsText = []
+
+        # Remove the spaces from the reviews text
+        for review in reviews:
+            reviewsText.append(review.text.replace("\n", " ").replace("\r", " ").replace("Helpfulread more", "").replace("                  ", " ").replace("                              ", " ").replace("|", ""))
+    except:
+        votes = []
+        reviewsText = []
+        print("Error in votes and reviews for id: " + str(id))
+    return votes, reviewsText
 
 # Write the recommendations in a csv file
 def recommendationsCsv(id, recommendations):
@@ -86,29 +119,27 @@ def recommendationsCsv(id, recommendations):
             file.write(str(id) + ',' + str(recommendation) + "\n")
     file.close()
 
-# Write the reviews in a csv file
-def reviewsCsv(id, reviews, votes):
+def reviewsCsv(id, votes, reviews):
     with open("../data/scraping/reviews.csv", "a") as file:
-        for review, vote in reviews, votes:
-            file.write(str(id) + ',' + str(vote) + ',' + review + "\n")
+        for i in range(len(votes)):
+            file.write(str(id) + '|' + str(votes[i]) + '|' + str(reviews[i]) + "\n")
     file.close()
 
-# Remove all the spaces in string
-def format(string):
-    return string.replace("\n", "").replace("\r", "")
 
 if __name__ == "__main__":
     # Get the page from every element in animeList
-    #ids = getMalIds()
-    _,_, content = getPage(22689)
-    informationsCsv(22689, getStudios(content), getProducer(content))
-    #for id in ids[1:]:
-        #statusCode, url, content = getPage(id)
-        #if checkErrors(statusCode, url, id):
-            #informationsCsv(id, getStudios(content), getProducer(content))
-            #recommendationsCsv(id, getRecommendations(content))
-            #reviewsCsv(id, getReviews(content))
-        #else:
-            #print("Error in page: " + str(id))
+    ids = getMalIds()
+    iteration = 0
+    for id in ids[1:]:
+        statusCode, url, content = getPage(id)
+        if checkErrors(statusCode, url, id):
+            informationsCsv(id, getStudios(content), getProducer(content))
+            recommendationsCsv(id, getRecommendations(id, content))
+            votes, reviews = getRatingAndReviews(content)
+            reviewsCsv(id, votes, reviews)
+            print("Written correctly anime number {}; id: ".format(iteration) + str(id) + "; url: " + url)
+            iteration += 1
+        else:
+            print("Error in page: " + str(id))
     
 
